f90cc030587578ba2ce9cb9194a80b3e75a0f28e
Paco Mendes <paco.mendes@spandigital.com>
2017-01-27 15:47:27 +0200

    Switched to bootstrap menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,46 +3,80 @@ import ReactDOM from 'react-dom';
 
 class Menu extends Component {
 
+    path(target) {
+        return this.props.baseurl + target;
+    }
+
     render() {
         return (
-            <nav id="sidenav">
-                <h2>DOCUMENTATION</h2>
-                <ul className="nav">
-                    <li>
-                        <a href="/#overview">Overview</a>
-                    </li>
-                    <li>
-                        <a href="/key-concepts#key-concepts">Key Concepts</a>
-                    </li>
-                    <li>
-                        <a href="/prerequisites#prerequisites">Prerequisites</a>
-                    </li>
-                    <li>
-                        <a href="/getting-started#getting-started">Getting started</a>
-                    </li>
-                    <li>
-                        <a href="/references#reference">Reference</a>
-                    </li>
-                    <li>
-                        <a href="/cookbook#cookbook">Cookbook</a>
-                    </li>
-                    <li>
-                        <a href="/tools#tools">Tools</a>
-                    </li>
-                    <li>
-                        <a href="/testing#testing-debugging">Testing & Debugging</a>
-                    </li>
-                    <li>
-                        <a href="/updates#updates">Updates</a>
-                    </li>
-                </ul>
+            //TODO Break up into components and generate dynamically
+            <nav className="navbar navbar-default navbar-fixed-side">
+
+                <div className="container">
+                    <div className="navbar-header">
+                        <a href="./" className="navbar-brand">
+                            <img src={this.path("/media/images/logo.png")} alt="" />
+                        </a>
+
+                        <button className="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                            <span className="icon-bar" />
+                        </button>
+                    </div>
+
+                    <div className="collapse navbar-collapse">
+                        <ul className="nav navbar-nav">
+                            <li className="active">
+                                <a href={ this.path("/") }>Overview</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/key-concepts") }>Key Concepts</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/prerequisites")}>Prerequisites</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/getting-started")}>Getting started</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/references")}>Reference</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/cookbook")}>Cookbook</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/tools")}>Tools</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/testing")}>Testing & Debugging</a>
+                            </li>
+                            <li>
+                                <a href={ this.path("/updates")}>Updates</a>
+                            </li>
+
+                            <li className="dropdown">
+                                <a className="dropdown-toggle" data-toggle="dropdown" href="#">References <b className="caret" /></a>
+                                <ul className="dropdown-menu"><li><a href="#">Sub-page 1</a></li>
+                                    <li><a href="#">Sub-page 2</a></li>
+                                    <li className="divider" />
+                                    <li className="dropdown-header">Sub Header</li>
+                                    <li><a href="#">Sub-page 3</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
             </nav>
         )
     }
+
 }
 
-function loadMenu(element = 'nav-container') {
-    ReactDOM.render(<Menu/>, document.getElementById(element));
+function loadMenu(baseurl = "/", element = 'nav-container') {
+    ReactDOM.render(<Menu baseurl={ baseurl }/>, document.getElementById(element));
 }
 
 export {Menu, loadMenu};
