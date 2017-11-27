075f962418053e09658729708fcc4ba3515b234d
Ruaan van der Spuy <rrvds87@gmail.com>
2017-11-27 10:07:25 +0200

    SPP-333 | Upgrade to Webpack v3 + optimise bundle size

--- etc/webpack.config.prod.js
+++ etc/webpack.config.prod.js
@@ -1,27 +1,38 @@
-var path = require('path');
+var webpack = require('webpack')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path:  path.join(__dirname, '../dist'),
+        path: path.join(__dirname, '../dist'),
         filename: 'presidium.js',
         publicPath: '/'
     },
     module: {
-        loaders: [
+        rules: [
             {
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
                     presets: ['react', 'es2015']
-                }}
+                }
+            }
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     devServer: {
         historyApiFallback: true,
         contentBase: '/'
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+        new UglifyJsPlugin()
+    ]
 };
--- src/util/slug.js
+++ src/util/slug.js
@@ -1,5 +1,8 @@
-const slug = require('slug');
-
 export function slugify(value) {
-    return slug(value, { mode: 'rfc3986' });
+    return value.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
 }
