03cabf6b9e9a7d9a1470214baffbaddcb432d9e5
DonnieTD <donovan@Donovans-MacBook-Pro.local>
2022-03-31 12:57:41 +0200

    added role filtering

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {loadTooltips} from './components/tooltips/tooltips';
 import { mountContainerListeners } from './util/articles';
 // import {handleQueryString, checkSessionStorageConfig} from './util/config';
 import {Subject} from 'rxjs/Subject'
+import $ from 'jquery';
 
 initModal();
 // TODO: Find a solution that is easier on the local storage if needed for edit mode.
@@ -29,3 +30,43 @@ var presidium = {
 
 window.presidium = presidium;
 window.events = new Subject();
+
+// Role filtering
+$(document).ready(function () {
+    const filterArticles = (selectedRole) => {
+        sessionStorage.setItem('role', selectedRole);
+        $('.article').show()
+        $('#presidium-navigation .menu-row').removeClass('hidden').show()
+        if (selectedRole != 'All Roles') {
+            const $articles = $(`.article:not([data-roles="All Roles"],[data-roles="${selectedRole}"])`)
+            $articles.each((i, article) => {
+                $(article).hide();
+                $(`#presidium-navigation .menu-row>a[data-target="#${article.id}"]`)
+                    .parent()
+                    .addClass('hidden')
+                    .hide();
+            });
+
+            $('#presidium-navigation .menu-row').each((_, e) => {
+                const $row = $(e)
+                const $children = $row.find('ul>li.menu-row');
+                const $active = $children.filter(':not(.hidden)');
+                if ($active.length == 0 && $children.length > 0) {
+                    $row.hide();
+                }
+            });
+        }
+    };
+
+    const lastRole = sessionStorage.getItem('role');
+    if (lastRole) {
+        $('#roles-select').val(lastRole);
+        filterArticles(lastRole);
+    }
+
+    $('#roles-select').on('change', function (e) {
+        const optionSelected = $("option:selected", this);
+        const selectedRole = this.value;
+        filterArticles(selectedRole);
+    });
+})
