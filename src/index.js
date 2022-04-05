cc6478c3d95ac931d4545e68cf8e6a12e931dad7
Lee Opperman <lee.opperman@spandigital.com>
2022-04-05 23:44:09 +0200

    Remove article events from presidium-js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {init as initModal} from './components/image/modal';
-import {loadTooltips} from './components/tooltips/tooltips';
+import { init as initModal } from './components/image/modal';
+import { loadTooltips } from './components/tooltips/tooltips';
 import { mountContainerListeners } from './util/articles';
 // import {handleQueryString, checkSessionStorageConfig} from './util/config';
-import {Subject} from 'rxjs/Subject'
+import { Subject } from 'rxjs/Subject';
 import $ from 'jquery';
 
 initModal();
@@ -15,16 +15,13 @@ initModal();
 // checkSessionStorageConfig(); //Check sessionStorage for known configurations and apply them
 
 var presidium = {
-    tooltips: {
-        load: loadTooltips,
-    },
-    articles: {
-        mount: mountContainerListeners,
-    },
-    modal: {
-        init: initModal,
-    },
-    versions: {}
+  tooltips: {
+    load: loadTooltips,
+  },
+  modal: {
+    init: initModal,
+  },
+  versions: {},
 };
 
 window.presidium = presidium;
@@ -32,40 +29,42 @@ window.events = new Subject();
 
 // Role filtering
 $(document).ready(function () {
-    const filterArticles = (selectedRole) => {
-        sessionStorage.setItem('role', selectedRole);
-        $('.article').show()
-        $('#presidium-navigation .menu-row').removeClass('hidden').show()
-        if (selectedRole != 'All Roles') {
-            const $articles = $(`.article:not([data-roles="All Roles"],[data-roles="${selectedRole}"])`)
-            $articles.each((i, article) => {
-                $(article).hide();
-                $(`#presidium-navigation .menu-row>a[data-target="#${article.id}"]`)
-                    .parent()
-                    .addClass('hidden')
-                    .hide();
-            });
+  const filterArticles = (selectedRole) => {
+    sessionStorage.setItem('role', selectedRole);
+    $('.article').show();
+    $('#presidium-navigation .menu-row').removeClass('hidden').show();
+    if (selectedRole != 'All Roles') {
+      const $articles = $(
+        `.article:not([data-roles="All Roles"],[data-roles="${selectedRole}"])`
+      );
+      $articles.each((i, article) => {
+        $(article).hide();
+        $(`#presidium-navigation .menu-row>a[data-target="#${article.id}"]`)
+          .parent()
+          .addClass('hidden')
+          .hide();
+      });
 
-            $('#presidium-navigation .menu-row').each((_, e) => {
-                const $row = $(e)
-                const $children = $row.find('ul>li.menu-row');
-                const $active = $children.filter(':not(.hidden)');
-                if ($active.length == 0 && $children.length > 0) {
-                    $row.hide();
-                }
-            });
+      $('#presidium-navigation .menu-row').each((_, e) => {
+        const $row = $(e);
+        const $children = $row.find('ul>li.menu-row');
+        const $active = $children.filter(':not(.hidden)');
+        if ($active.length == 0 && $children.length > 0) {
+          $row.hide();
         }
-    };
-
-    const lastRole = sessionStorage.getItem('role');
-    if (lastRole) {
-        $('#roles-select').val(lastRole);
-        filterArticles(lastRole);
+      });
     }
+  };
 
-    $('#roles-select').on('change', function (e) {
-        const optionSelected = $("option:selected", this);
-        const selectedRole = this.value;
-        filterArticles(selectedRole);
-    });
-})
+  const lastRole = sessionStorage.getItem('role');
+  if (lastRole) {
+    $('#roles-select').val(lastRole);
+    filterArticles(lastRole);
+  }
+
+  $('#roles-select').on('change', function (e) {
+    const optionSelected = $('option:selected', this);
+    const selectedRole = this.value;
+    filterArticles(selectedRole);
+  });
+});
--- src/util/articles.js
+++ src/util/articles.js
@@ -1,91 +1,30 @@
-import {ACTIONS, EVENTS_DISPATCH} from './events';
-
-const mountContainerListeners = () => {
-    const scrollPause = 2000;
-    let timeout;
-    window.addEventListener('scroll', () => {
-        clearTimeout(timeout);
-        timeout = setTimeout(findArticlesInView, scrollPause);
-    })
-}
-
-const findArticlesInView = () => {
-    let articles = [...document.querySelectorAll('.article')];
-    articles.map((article) => {
-        if (isInViewport(article)) {
-            const articleId = article.querySelector('span[data-id]').getAttribute('data-id');
-            let permalink = article.querySelector('.permalink a')
-            // Section titles do not have a permalink
-            if (permalink) {
-                markArticleAsViewed(articleId, permalink.getAttribute('href'), ACTIONS.articleScroll);
-            }
-        }
-    })
-}
-
-const markArticleAsViewed = (articleId, permalink = null, action = ACTIONS.articleScroll) => {
-    const cachedSolution = sessionStorage.getItem('presidium.solution');
-    if (!cachedSolution) return;
-
-    const hash = `PRESIDIUM-ACTION:${articleId}:${cachedSolution || ''}`;
-    const cachedAction = sessionStorage.getItem(hash)
-
-    if (!cachedAction) {
-        EVENTS_DISPATCH.ARTICLE(articleId, permalink, action, cachedSolution);
-        sessionStorage.setItem(hash, action)
-    }
-};
-
 /**
  * Concatenate uri with single slash
  */
 const path = {
-    concat: function (base, target) {
-        return base +
-            path.forceTrailing(base) +
-            path.removeLeading(target);
-    },
-    forceTrailing: function (path) {
-        return path == null ? '/' : (path.substr(-1) != '/' ? '/' : '');
-    },
-    removeLeading: function (path) {
-        return path == null ? '' : (path.substr(0, 1) == '/' ? path.substr(1) : path)
-    }
+  concat: function (base, target) {
+    return base + path.forceTrailing(base) + path.removeLeading(target);
+  },
+  forceTrailing: function (path) {
+    return path == null ? '/' : path.substr(-1) != '/' ? '/' : '';
+  },
+  removeLeading: function (path) {
+    return path == null ? '' : path.substr(0, 1) == '/' ? path.substr(1) : path;
+  },
 };
 
 /**
  * Generic replacement for NPM slug lib
  */
 const slugify = (value) => {
-    return value.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
+  return value
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
 };
 
-/**
- * Calculate if article is within viewport dimensions
- * @param element
- * @returns {boolean}
- */
-
- const isInViewport = (element) => {
-    const rect = element.getBoundingClientRect();
-    const height = window.innerHeight
-    if(rect.top < 0) {
-        return rect.height + rect.top > height;
-    }
-    return rect.top + rect.height < height;
-}
-
-window.isInViewport = isInViewport;
-
-export {
-    path,
-    slugify,
-    isInViewport,
-    markArticleAsViewed,
-    mountContainerListeners
-}
+export { path, slugify };
