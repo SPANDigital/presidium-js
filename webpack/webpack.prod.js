41a1e5e87d8709cff821f7a5fe7bfce0519537e7
Helgard <48946187+meyerhp@users.noreply.github.com>
2022-06-07 00:03:36 +0200

    Fix/prs 2308 (#79)
    * updated deps
    
    * fix(PRS-2308): update webpack and added gzip
    
    * fix(PRS-2308): update build name and path
    
    * fix(PRS-2308): only zip js and ts
    
    * fix(PRES-2308): revert build path
    
    * fix(PRS-2308): enable static build for bundle analyzer
    
    * Update package json
    
    * Revert "fix(PRS-2308): enable static build for bundle analyzer"
    
    This reverts commit cc4611e0b2f89b039beedf56593833de89b3c640.
    
    * Update build path and review bundle analyzer
    
    Co-authored-by: Andres Felipe De Castro <af.decastro879@gmail.com>
    Co-authored-by: Ruaan van der Spuy <rrvds87@gmail.com>
    Co-authored-by: Tayla Geben <tayla.geben@spandigital.com>
    Co-authored-by: En Zhou <zhouen.nathan@gmail.com>
    Co-authored-by: Zelre Stemmet <zelre.stemmet@gmail.com>
    Co-authored-by: DonnieTD <donovan@Donovans-MacBook-Pro.local>

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+const CompressionPlugin = require("compression-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new CompressionPlugin({
+            filename: "[path][name].jgz",
+            test: /\.(ts|js)x?$/,
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false
+        }),
+    ],
+}
