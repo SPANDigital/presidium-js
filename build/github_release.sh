dc41ae2137792b6138a060ef99c5c0720cd50b9a
Helgard <48946187+meyerhp@users.noreply.github.com>
2023-10-03 23:16:41 +0200

    Feat/release (#112)
    * PRSDM-4416
    
    Ensured that only the defined targets get identified
    
    * Set feat releases
    
    * Update repo url
    
    * Remove main from branch list
    
    ---------
    
    Co-authored-by: wiaan <wiaan.botha@spandigital.com>
    Co-authored-by: Travis CI <build@travis-ci.com>

--- build/github_release.sh
+++ build/github_release.sh
@@ -8,33 +8,38 @@ source "${DIR}/include.sh"
 # This script will create a release on GitHub with build artifacts attached
 # To run this script manually, you can use the following command:
 # TRAVIS_TAG=your_release_version GITHUB_KEY=your_github_key ./build/github_release.sh
-# Before running this script, you will need to run "npm run build" 
+# Before running this script, you will need to run "npm run build"
 
-if [[ -n "${TRAVIS_TAG}" && ${TRAVIS_TAG} =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
-then
-    echo "Creating release on GitHub for tag ${TRAVIS_TAG}"
+if [[ -n "${TRAVIS_TAG}" ]]; then
+    # Check if the branch starts with "feat/"
+    if [[ "${TRAVIS_TAG}" =~ feat- ]]; then
+        echo "Creating pre-release on GitHub for tag ${TRAVIS_TAG}"
+        prerelease=true
+    else
+        echo "Creating release on GitHub for tag ${TRAVIS_TAG}"
+        prerelease=false
+    fi
+
     zip_file=$TRAVIS_TAG.zip
 
     # Zip the dist folder
     zip -r -qq $zip_file dist
 
     # Create a release on GitHub
-    release=$(curl -s -d "{\"tag_name\": \"$TRAVIS_TAG\"}" -H "Authorization: token ${GITHUB_KEY}" -X POST "https://api.github.com/repos/SPANDigital/presidium-js/releases")
+    release=$(curl -s -d "{\"tag_name\": \"$TRAVIS_TAG\", \"prerelease\": $prerelease}" -H "Authorization: token ${GITHUB_KEY}" -X POST "https://api.github.com/repos/SPANDigital/presidium-js/releases")
 
-    if [[ $(echo $release | jq -r '.message') = "Not Found" ]]
-    then
+    if [[ $(echo $release | jq -r '.message') = "Not Found" ]]; then
         echo "Error creating release on GitHub"
         exit 1
     fi
 
     # Get the upload url from the release
     upload_url=$(echo $release | jq -r '.upload_url')
-    
+
     # Remove the {?name,label} from the upload_url
     upload_url="${upload_url//\{?name,label\}/}"
 
-    if [[ -z "$upload_url" ]]
-    then
+    if [[ -z "$upload_url" ]]; then
         echo "Error getting upload url from GitHub"
         exit 1
     fi
